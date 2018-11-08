24fa39d736386e04c7d1cf5d753459da74f51a6b
Alex Fu <alex.fu914@gmail.com>
2018-11-08 12:31:42 -0500

    Add GreetingPlugin

---
+++ buildSrc/src/main/groovy/GreeingPlugin.groovy
@@ -0,0 +1,13 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class GreetingPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.task("hello") {
+            doLast {
+                println("Hello from GreetingPlugin")
+            }
+        }
+    }
+}
